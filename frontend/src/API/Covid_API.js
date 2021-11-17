f7bd8d3c5e8c90a08c2f58a7fbc1b3c4c128179c
Nico Caro <nicolascaro926@gmail.com>
2021-11-17 17:23:29 +0100

    first commit : test

---
+++ frontend/src/API/Covid_API.js
@@ -0,0 +1,26 @@
+// API : https://openweathermap.org/api
+// Url API
+const API_URL = "https://api.covid19tracking.narrativa.com/api/";
+
+class Covid_API {
+  constructor(country, region, city) {
+    // Si la ville n'est pas définit alors la ville par défault est Paris
+    if (city === undefined) {
+      city = "paris";
+    }
+    if (country === undefined) {
+      country = "France";
+    }
+    this.country = country;
+    this.city = city;
+  }
+
+  // Faire la requete à l'API openweathermap
+  // Retourne une promise
+  GetInfoByCountry() {
+    return axios
+      .get(`${API_URL}${this.country}`, {
+        crossdomain: true
+      })
+  }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './API/script.js';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DONE
         </p>
         <a
           className="App-link"
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={'start()'}></button>
       </header>
     </div>
   );
