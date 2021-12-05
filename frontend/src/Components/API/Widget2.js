05b96ae06687e503954420198c9c965ad8d6a3ff
rorolasticot <61541135+rorolasticot@users.noreply.github.com>
2021-12-05 21:51:30 +0100

    widget2

--- frontend/src/Components/API/Widget2.js
+++ frontend/src/Components/API/Widget2.js
@@ -33,15 +33,9 @@ export default class Widget2 extends React.Component {
                     <div className="row">
                         <div className="petitcontainer">
                             <div className="column">
-<<<<<<< HEAD
                                 <div className="nbrVaccine">{this.state.post.people_fully_vaccinated}</div>
                                 <div className="nbrVaccineToday">+{this.state.post.new_vaccinations_smoothed}</div>
                                 <div className="nbrVaccineToday">Nombre de vaccinés (2 ou 3 doses)</div>
-=======
-                                <div className="nbrVaccines">{this.state.post.people_fully_vaccinated}</div>
-                                <div className="nbrVaccinesToday">+{this.state.post.new_vaccinations_smoothed}</div>
-                                <div className="nbrVaccinesToday">Nombre de vaccinés (2 ou 3 doses)</div>
->>>>>>> fec3ebb9a78cda97fb265980c8e1dde5faf2aedc
                             </div>
                         </div>
                     </div>
